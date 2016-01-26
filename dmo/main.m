f9ab195c7e0ae12e9b07a4ac28d941b4ce1f3d0c
fengyifande <1425277538@qq.com>
2016-01-26 18:04:56 +0800

    12121

---
+++ dmo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  dmo
+//
+//  Created by Qianfeng on 16/1/26.
+//  Copyright © 2016年 冯逸帆. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ dmoTests/dmoTests.m
@@ -0,0 +1,39 @@
+//
+//  dmoTests.m
+//  dmoTests
+//
+//  Created by Qianfeng on 16/1/26.
+//  Copyright © 2016年 冯逸帆. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface dmoTests : XCTestCase
+
+@end
+
+@implementation dmoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
